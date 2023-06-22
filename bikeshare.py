8de07a2b699f5f04934593770259dfcdfcb99f61
Nathan Horner <nathanhorner13@yahoo.com>
2023-06-22 11:16:05 -0400

    Deleted leftover hint comment

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,7 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
 
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington).
     valid_city = CITY_DATA.keys()
     city=''
     while city not in valid_city:
